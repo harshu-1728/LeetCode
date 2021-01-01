346d04d232ad5c3856a81b102a77393f1ed6148e
tushar-1728 <tusharraj.1728@gmail.com>
2021-01-01 18:26:47 +0530

    01-01-2021

--- Trapping Rain Water.cpp
+++ Trapping Rain Water.cpp
@@ -13,14 +13,14 @@ int min(int a, int b){
 }
 
 int trap_water(int n, int *arr){
-    vector <int> arr1, arr2;
+    int arr1[n], arr2[n];
     int m1, m2, water;
     water = m1 = m2 = 0;
     for (int i = 0; i < n; ++i){
         m1 = max(m1, arr[i]);
         m2 = max(m2, arr[n-i-1]);
-        arr1.push_back(m1);
-        arr2.insert(arr2.begin(), m2);
+        arr1[i] = m1;
+        arr2[n-i-1] = m2;
     }
     for (int i = 0; i < n; ++i){
         water += min(arr1[i], arr2[i]) - arr[i];
@@ -31,13 +31,13 @@ int trap_water(int n, int *arr){
 int main(int argc, char const *argv[])
 {
     int t;
-    cin >> t;
+    scanf("%d", &t);
     while (t--){
         int n;
-        cin >> n;
+        scanf("%d", &n);
         int arr[n];
         for (int i = 0; i < n; ++i)
-            cin >> arr[i];
+            scanf("%d", &arr[i]);
         cout << trap_water(n, arr) << endl;
     }
     return 0;
