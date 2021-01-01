a4b16190767f6e14bd0c6da10f195b16b3a3cce0
tushar-1728 <tusharraj.1728@gmail.com>
2021-01-01 20:19:12 +0530

    added Chocolate Distribution Problem.cpp

--- Pythagorean Triplet.cpp
+++ Pythagorean Triplet.cpp
@@ -11,7 +11,30 @@ public:
     // Function to check if the
     // Pythagorean triplet exists or not
     bool checkTriplet(int arr[], int n) {
-        // code here
+        int arr1[n];
+        sort(arr, arr+n);
+        for (int i = 0; i < n; ++i){
+            arr1[i] = arr[i] * arr[i];
+        }
+        int i, j, temp, sum;
+        temp = 2;
+        while (temp < n){
+            i = 0; j = temp-1;
+            while (i < j){
+                sum = arr1[i] + arr1[j];
+                if (sum == arr1[temp]){
+                    return true;
+                }
+                if (sum < arr1[temp]){
+                    i += 1;
+                }
+                else{
+                    j -= 1;
+                }
+            }
+            temp += 1;
+        }
+        return false;
     }
 };
 
