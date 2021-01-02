2267308f671e98a91d5fcd47b4953570dce3bfb5
tushar-1728 <tusharraj.1728@gmail.com>
2021-01-02 15:41:40 +0530

    added Rearrange an array with O(1) extra space.cpp

---
+++ Rearrange an array with O(1) extra space.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+// Function to rarrange the elements in O(1) extra space
+// arr: input array
+// n: size of array
+void arrange(long long arr[], int n) {
+    int maxe = n;
+    for (int i = 0; i < n; i++){
+        arr[i] += (arr[arr[i]] % maxe) * maxe;
+    }
+    for (int i = 0; i < n; ++i){
+        arr[i] = arr[i] / n;
+    }
+}
+
+
+// { Driver Code Starts.
+
+int main(){
+
+    int t;
+    //testcases
+    cin>>t;
+    while(t--){
+
+        int n;
+        //size of array
+        cin>>n;
+        long long A[n];
+
+        //adding elements to the array
+        for(int i=0;i<n;i++){
+            cin>>A[i];
+        }
+
+        //calling arrange() function
+        arrange(A, n);
+
+        //printing the elements
+        for(int i=0;i<n;i++){
+            cout << A[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
