f4ef6eccc1c7502af5a323e23d586f5a917a0e26
tushar-1728 <tusharraj.1728@gmail.com>
2021-01-03 13:32:30 +0530

    added Stock span problem.cpp

--- Stock span problem.cpp
+++ Stock span problem.cpp
@@ -1,5 +1,6 @@
 // { Driver Code Starts
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 
@@ -9,7 +10,35 @@ using namespace std;
 // n: size of array
 vector <int> calculateSpan(int price[], int n)
 {
-   // Your code here
+    // Create a stack and push index of first
+    // element to it
+    stack<int> st;
+    st.push(0);
+    vector <int> S(n, 0);
+    // Span value of first element is always 1
+    S[0] = 1;
+
+    // Calculate span values for rest of the elements
+    for (int i = 1; i < n; i++) {
+        // Pop elements from stack while stack is not
+        // empty and top of stack is smaller than
+        // price[i]
+        while (!st.empty() && price[st.top()] <= price[i]){
+            st.pop();
+        }
+
+        // If stack becomes empty, then price[i] is
+        // greater than all elements on left of it,
+        // i.e., price[0], price[1], ..price[i-1].  Else
+        // price[i] is greater than elements after
+        // top of stack
+        S[i] = (st.empty()) ? (i + 1) : (i - st.top());
+        // cout << "stack: " << st << endl;
+
+        // Push this element to stack
+        st.push(i);
+    }
+    return S;
 }
 
 // { Driver Code Starts.
