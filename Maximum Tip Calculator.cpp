0acbff50b6b2a280d8507573cc2ef2dc7e89ad2b
tushar-1728 <tusharraj.1728@gmail.com>
2021-01-04 17:54:27 +0530

    added for modificatio

---
+++ Maximum Tip Calculator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--){
+		int n, x, y;
+		cin >> n >> x >> y;
+		int arr1[n], arr2[n];
+		for (int i = 0; i < n; ++i){
+			cin >> arr1[i];
+		}
+		for (int i = 0; i < n; ++i){
+			cin >> arr2[i];
+		}
+
+	}
+	return 0;
+}
