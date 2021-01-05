de6786aaf30857cbebefd5bdfbc89fc62ecf2478
tushar-1728 <tusharraj.1728@gmail.com>
2021-01-05 13:21:44 +0530

    modified Find Nth Fibonacci.py

--- 05-01/Find Nth Fibonacci.py
+++ 05-01/Find Nth Fibonacci.py
@@ -1,25 +1,32 @@
 class Solution:
     # @param A : integer
     # @return an integer
+
+    def pow(self, a, n):
+        if n == 1:
+            return a
+        elif n%2 == 1:
+            return self.mul(self.mul(a, self.pow(a, n//2)), self.pow(a, n//2))
+        else:
+            return self.mul(self.pow(a, n//2), self.pow(a, n//2))
+
+    def mul(self, m1, m2):
+        temp = [[0, 0], [0, 0]]
+        mod = 1000000007
+        temp[0][0] = ((m1[0][0]*m2[0][0])%mod + (m1[0][1]*m2[1][0])%mod)%mod
+        temp[0][1] = ((m1[0][0]*m2[0][1])%mod + (m1[0][1]*m2[1][1])%mod)%mod
+        temp[1][0] = ((m1[1][0]*m2[0][0])%mod + (m1[1][1]*m2[1][0])%mod)%mod
+        temp[1][1] = ((m1[1][0]*m2[0][1])%mod + (m1[1][1]*m2[1][1])%mod)%mod
+        return temp
+
+
     def solve(self, A):
         if A == 1 or A == 2:
             return 1
-        mod = 1000000007
         mat = [[1, 1], [1, 0]]
-        i = 1
-        while i*i <= A-1:
-            if i == 1:
-                i = 2
-            else:
-                i = i*i
-            temp = [[0, 0], [0, 0]]
-            for i in range(len(mat)):
-                for j in range(mat[0]):
-                    for k in range():
-                        temp[i][j] += (mat[i][k] % mod * mat[k][j] % mod) % mod
-            mat = temp
-        if i == A-1:
-            return mat[0][0]
+        i = A-1
+        temp = self.pow(mat, i)
+        return temp[0][0]
 
 
 n = int(input())
--- Find Nth Fibonacci.py
+++ Find Nth Fibonacci.py
@@ -1,31 +1,32 @@
 class Solution:
     # @param A : integer
     # @return an integer
+
+    def pow(self, a, n):
+        if n == 1:
+            return a
+        elif n%2 == 1:
+            return self.mul(self.mul(a, self.pow(a, n//2)), self.pow(a, n//2))
+        else:
+            return self.mul(self.pow(a, n//2), self.pow(a, n//2))
+
+    def mul(self, m1, m2):
+        temp = [[0, 0], [0, 0]]
+        mod = 1000000007
+        temp[0][0] = ((m1[0][0]*m2[0][0])%mod + (m1[0][1]*m2[1][0])%mod)%mod
+        temp[0][1] = ((m1[0][0]*m2[0][1])%mod + (m1[0][1]*m2[1][1])%mod)%mod
+        temp[1][0] = ((m1[1][0]*m2[0][0])%mod + (m1[1][1]*m2[1][0])%mod)%mod
+        temp[1][1] = ((m1[1][0]*m2[0][1])%mod + (m1[1][1]*m2[1][1])%mod)%mod
+        return temp
+
+
     def solve(self, A):
         if A == 1 or A == 2:
             return 1
-        mod = 1000000007
         mat = [[1, 1], [1, 0]]
-        i = 1
-        while i*2 <= A-1:
-            i = i * 2
-            temp = [[0, 0], [0, 0]]
-            for t in range(2):
-                for j in range(2):
-                    for k in range(2):
-                        temp[t][j] += (mat[t][k] % mod * mat[k][j] % mod) % mod
-            mat = list(temp)
-        if i == A-1:
-            return mat[0][0]
-        i += 1
-        curr = mat[0][0]
-        prev = mat[0][1]
-        while i < A:
-            temp = (curr % mod + prev % mod) % mod
-            prev = curr
-            curr = temp
-            i += 1
-        return curr
+        i = A-1
+        temp = self.pow(mat, i)
+        return temp[0][0]
 
 
 n = int(input())
